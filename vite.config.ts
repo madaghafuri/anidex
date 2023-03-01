bd5b105737fd6279e118fad4413b7e3ae199817e
madaghafuri <muhammadghafuri6301@gmail.com>
2023-03-01 19:53:11 +0700

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+        react(),
+        svgr(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+            manifest: {
+                name: 'AniDex - AniList Client',
+                short_name: 'AniDex',
+                description:
+                    'Discover and browse anime. Add anime to you collections',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'vite.svg',
+                        sizes: '192x192',
+                        type: 'svg',
+                    },
+                    {
+                        src: 'vite.svg',
+                        sizes: '512x512',
+                        type: 'svg',
+                    },
+                ],
+                background_color: '#ffffff',
+            },
+        }),
+    ],
 });
